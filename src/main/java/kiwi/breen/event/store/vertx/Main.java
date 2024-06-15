b3bbcb0b1df45b620ec18e313380a54717e6a567
Nicholas Breen <nick@breen.kiwi>
2024-06-15 14:48:14 +1200

    logging

--- src/main/java/kiwi/breen/event/store/vertx/Main.java
+++ src/main/java/kiwi/breen/event/store/vertx/Main.java
@@ -1,11 +1,17 @@
 package kiwi.breen.event.store.vertx;
 
 import io.vertx.core.Vertx;
+import org.slf4j.Logger;
 
 public class Main
 {
+    private static final Logger LOGGER = org.slf4j.LoggerFactory.getLogger(Main.class);
+
     public static void main(final String[] args)
     {
-        Vertx.vertx().deployVerticle(new EventStoreVerticle());
+        Vertx.vertx().deployVerticle(new EventStoreVerticle()).onComplete(
+                x -> LOGGER.info("Verticle started {}", x),
+                cause -> LOGGER.error("Verticle failed to start", cause)
+        );
     }
 }
